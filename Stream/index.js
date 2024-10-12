f27d02e408bfba7e472719726038f4f435727e18
jagpreetrahi <jagpreet1615@gmail.com>
2024-10-12 14:03:53 +0530

    Change the data for user

--- Stream/index.js
+++ Stream/index.js
@@ -5,9 +5,9 @@ const path = new URL('./index.html' , import.meta.url );
 let content = await  readFile(path , {encoding : 'utf-8'});
 
 const data = {
-    name : "Jagpreet Singh",
-    role : "Developer",
-    place: "Pune"
+    name : "Your Name",
+    role : "Your Role",
+    place: " Your location"
 }
 
 for(const [key , value] of Object.entries(data)){
